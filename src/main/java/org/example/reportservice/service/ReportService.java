9324fec5fd388cf5be994e54843471cb1282ed5d
alyvnihad <aliyevnihad011@gmail.com>
2025-11-12 21:34:52 +0400

    ReportService update.v1

--- src/main/java/org/example/reportservice/service/ReportService.java
+++ src/main/java/org/example/reportservice/service/ReportService.java
@@ -4,13 +4,29 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.reportservice.dto.ReportRequest;
+import org.example.reportservice.dto.TransactionRequest;
+import org.example.reportservice.dto.TransactionResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
 
 /**
  * Service class responsible for generating PDF reports
@@ -19,7 +35,14 @@ import java.io.FileOutputStream;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReportService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${transaction.url}")
+    private String transactionUrl;
+
     @Value("${folder.path}")
     private String folderPath;
 
@@ -58,4 +81,57 @@ public class ReportService {
             throw new RuntimeException(e);
         }
     }
+
+    public String dailyLog(LocalDate date ,TransactionRequest request) throws IOException {
+        request.setDate(date);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(request.getRefreshToken());
+        HttpEntity<TransactionRequest> entity = new HttpEntity<>(request, httpHeaders);
+
+        ResponseEntity<TransactionResponse> response =
+                restTemplate.postForEntity(transactionUrl + "/daily-log", entity, TransactionResponse.class);
+        String folderPath = this.folderPath;
+        File file = new File(folderPath);
+        if (file.exists()) {
+            file.mkdirs();
+        }
+        List<TransactionResponse> list = Collections.singletonList(response.getBody());
+        if (response.getBody() != null) {
+
+            String filePath = folderPath + "\\Daily-log sheet-" + response.getBody().getDateTime().toString() + ".xlsx";
+            String[] columns = {"Account_ID", "ATM_ID", "Card Number", "Amount", "Type", "Created_at"};
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Daily Log");
+            Font font = workbook.createFont();
+            font.setBold(true);
+
+            Row headerRow = sheet.createRow(0);
+
+            for (int i = 0; i < columns.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(columns[i]);
+            }
+
+            int rowIndex = 1;
+            for (TransactionResponse result : list) {
+                Row row = sheet.createRow(rowIndex++);
+                row.createCell(0).setCellValue(result.getAccountId());
+                row.createCell(1).setCellValue(result.getAtmId());
+                row.createCell(2).setCellValue(result.getCardNumber());
+                row.createCell(3).setCellValue(result.getAmount());
+                row.createCell(4).setCellValue(result.getType());
+                row.createCell(5).setCellValue(result.getDateTime());
+            }
+
+            try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
+                workbook.write(fileOutputStream);
+            }
+            workbook.close();
+            return filePath;
+        }
+
+        log.error("Error!");
+        return null;
+    }
+
 }
