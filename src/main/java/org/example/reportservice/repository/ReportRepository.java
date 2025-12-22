eb1d3fee5722c466b96b64b1809170862549daed
alyvnihad <aliyevnihad011@gmail.com>
2025-12-22 22:38:31 +0400

    Report Repository update.v1 repository class

--- src/main/java/org/example/reportservice/repository/ReportRepository.java
+++ src/main/java/org/example/reportservice/repository/ReportRepository.java
@@ -3,8 +3,11 @@ package org.example.reportservice.repository;
 import org.example.reportservice.model.CustomerReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<CustomerReport,Long> {
     Optional<CustomerReport> findByAccountIdAndType(Long accountId, String type);
+    List<CustomerReport> findTop10ByCreatedAtBetweenOrderByAmountDesc(LocalDateTime start, LocalDateTime end);
 }
